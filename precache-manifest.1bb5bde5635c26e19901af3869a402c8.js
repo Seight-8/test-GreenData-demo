a2ecda28e0bd45e7b09e77d2aa24aa44bc6b7352
Ivan <shelkovnikov_i.a@mail.ru>
2020-03-02 20:40:33 +0500

    Updates

---
+++ precache-manifest.1bb5bde5635c26e19901af3869a402c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd42221a9653d6d7c154bdb40a293d5",
+    "url": "/test-GreenData-demo/index.html"
+  },
+  {
+    "revision": "0ee32a715e58a4eda005",
+    "url": "/test-GreenData-demo/static/css/main.716bce0d.chunk.css"
+  },
+  {
+    "revision": "eb5ccdbe46f472918b93",
+    "url": "/test-GreenData-demo/static/js/2.b4aed19b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test-GreenData-demo/static/js/2.b4aed19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee32a715e58a4eda005",
+    "url": "/test-GreenData-demo/static/js/main.f727fa64.chunk.js"
+  },
+  {
+    "revision": "ea0e84e36595e8c3f0d7",
+    "url": "/test-GreenData-demo/static/js/runtime-main.d188bdfb.js"
+  }
+]);
